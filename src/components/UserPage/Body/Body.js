e9a4c3746219fbe80370611fdd5bd584d833bd64
Horacio Ramírez <horacioram94@gmail.com>
2019-04-22 17:14:21 +0100

    Mejora de estilos en la página usuario, en la tabla de ejercicios

--- src/components/UserPage/Body/Body.js
+++ src/components/UserPage/Body/Body.js
@@ -6,7 +6,7 @@ export default class Body extends Component {
     render() {
         return (
             <section id="routine">
-                <div className="p-5">
+                <div className="p-5 routine-container">
                     <ul className="nav nav-tabs" role="tablist">
                         <li className="nav-item">
                             <a className="nav-link active" data-toggle="tab" href="#monday">Lunes</a>
@@ -31,7 +31,7 @@ export default class Body extends Component {
                         </li>
                     </ul>
 
-                    <div className="tab-content">
+                    <div className="tab-content routineWrapper col-9 m-auto pt-5 pl-5 pr-5">
 
                         {/* MONDAY */}
                         <div id="monday" className="container tab-pane active"><br />
@@ -82,6 +82,9 @@ export default class Body extends Component {
                             </div>
                         </div>
                     </div>
+
+                    {/* Skew div */}
+                    <div className="skew"></div>
                 </div>
             </section >
         );
--- src/components/UserPage/Body/Routine/Routine.js
+++ src/components/UserPage/Body/Routine/Routine.js
@@ -7,64 +7,63 @@ export default class Routine extends Component {
 
     render() {
         return (
-            <table className="table table-borderless table-dark table-hover table-striped routine">
-                <thead>
-                    <tr>
-                        <td colSpan="4" className="dark-green">Calentamiento</td>
-                    </tr>
-                    <tr>
-                        <td colSpan="2" className="ligth-green">{this.props.routine[0].name}</td>
-                        <td colSpan="2">{this.props.routine[0].time}</td>
-                    </tr>
-                    <tr>
-                        <td colSpan="4" className="dark-green">Rutina de {this.props.routine.routine}</td>
-                    </tr>
-                    <tr className="ligth-green">
-                        <td>Ejercicio</td>
-                        <td>Series</td>
-                        <td>Repeticiones</td>
-                        <td>Descanso</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{this.props.routine[1].name}</td>
-                        <td>{this.props.routine[1].series}</td>
-                        <td>{this.props.routine[1].loops}</td>
-                        <td>{this.props.routine[1].rest}</td>
-                    </tr>
-                    <tr>
-                        <td>{this.props.routine[2].name}</td>
-                        <td>{this.props.routine[2].series}</td>
-                        <td>{this.props.routine[2].loops}</td>
-                        <td>{this.props.routine[2].rest}</td>
-                    </tr>
-                    <tr>
-                        <td>{this.props.routine[3].name}</td>
-                        <td>{this.props.routine[3].series}</td>
-                        <td>{this.props.routine[3].loops}</td>
-                        <td>{this.props.routine[3].rest}</td>
-                    </tr>
-                    <tr>
-                        <td>{this.props.routine[4].name}</td>
-                        <td>{this.props.routine[4].series}</td>
-                        <td>{this.props.routine[4].loops}</td>
-                        <td>{this.props.routine[4].rest}</td>
-                    </tr>
-                    <tr>
-                        <td>{this.props.routine[5].name}</td>
-                        <td>{this.props.routine[5].series}</td>
-                        <td>{this.props.routine[5].loops}</td>
-                        <td>{this.props.routine[5].rest}</td>
-                    </tr>
-                    <tr>
-                        <td>{this.props.routine[6].name}</td>
-                        <td>{this.props.routine[6].series}</td>
-                        <td>{this.props.routine[6].loops}</td>
-                        <td>{this.props.routine[6].rest}</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div className="routine">
+                <table className="table table-borderless table-hover table-striped">
+                    <thead>
+                        <tr className="header">
+                            <td colSpan="4" className="dark-green">Rutina de {this.props.routine.routine}</td>
+                        </tr>
+                        <tr className="warm-up">
+                            <td colSpan="2" className="ligth-green">{this.props.routine[0].name}</td>
+                            <td colSpan="2">{this.props.routine[0].time}</td>
+                        </tr>
+                        <tr className="title">
+                            <td>Ejercicio</td>
+                            <td>Series</td>
+                            <td>Repeticiones</td>
+                            <td>Descanso</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{this.props.routine[1].name}</td>
+                            <td>{this.props.routine[1].series}</td>
+                            <td>{this.props.routine[1].loops}</td>
+                            <td>{this.props.routine[1].rest}</td>
+                        </tr>
+                        <tr>
+                            <td>{this.props.routine[2].name}</td>
+                            <td>{this.props.routine[2].series}</td>
+                            <td>{this.props.routine[2].loops}</td>
+                            <td>{this.props.routine[2].rest}</td>
+                        </tr>
+                        <tr>
+                            <td>{this.props.routine[3].name}</td>
+                            <td>{this.props.routine[3].series}</td>
+                            <td>{this.props.routine[3].loops}</td>
+                            <td>{this.props.routine[3].rest}</td>
+                        </tr>
+                        <tr>
+                            <td>{this.props.routine[4].name}</td>
+                            <td>{this.props.routine[4].series}</td>
+                            <td>{this.props.routine[4].loops}</td>
+                            <td>{this.props.routine[4].rest}</td>
+                        </tr>
+                        <tr>
+                            <td>{this.props.routine[5].name}</td>
+                            <td>{this.props.routine[5].series}</td>
+                            <td>{this.props.routine[5].loops}</td>
+                            <td>{this.props.routine[5].rest}</td>
+                        </tr>
+                        <tr>
+                            <td>{this.props.routine[6].name}</td>
+                            <td>{this.props.routine[6].series}</td>
+                            <td>{this.props.routine[6].loops}</td>
+                            <td>{this.props.routine[6].rest}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         );
     }
 }
